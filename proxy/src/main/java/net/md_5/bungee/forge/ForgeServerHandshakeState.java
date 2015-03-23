d965b654e2d3d911c3e061e6d68b0c2101833a67
devryb <bradydevrye@gmail.com>
2015-03-23 14:51:29 -0400

    Update ForgeServerHandshakeState.java

--- proxy/src/main/java/net/md_5/bungee/forge/ForgeServerHandshakeState.java
+++ proxy/src/main/java/net/md_5/bungee/forge/ForgeServerHandshakeState.java
@@ -29,6 +29,9 @@ public enum ForgeServerHandshakeState implements IForgeServerPacketHandler<Forge
                 @Override
                 public ForgeServerHandshakeState send(PluginMessage message, UserConnection con)
                 {
+                    // Packets should never make it here but if they ever do, pass everything to client
+                    ForgeLogger.logServer( LogDirection.SENDING, this.name(), message);
+                    con.unsafe().sendPacket(message);
                     // Send custom channel registration. Send Hello.
                     return HELLO;
                 }
